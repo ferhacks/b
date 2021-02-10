24f7129d3a584758bd000bcfc6ff05152472b32c
ferhacks <fernandovillaverde40@gmail.com>
2021-02-10 00:50:11 -0300

    megass

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 # Do an inline sync first, then start the background job
 echo "Starting sync..."
-back/sync
+back/sync.sh
 if [ "$READ_ONLY" != "true" ]; then
-  eval "while true; do sleep ${AWS_SYNC_INTERVAL:-60}; back/sync; done &"
+  eval "while true; do sleep ${AWS_SYNC_INTERVAL:-60}; back/sync.sh; done &"
   sync_pid=$!
 fi
 
